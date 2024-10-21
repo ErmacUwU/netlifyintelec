bcd869ac248a3f6bb7737f8c7050ac795dbb2c03
Victor <al20760334@ite.edu.mx>
2024-10-21 11:40:10 -0700

    Cambios

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+
 const Contact = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-200">
-      <div className="w-full max-w-4xl bg-white rounded-lg shadow-lg p-8">
+    <div className="flex items-center justify-center min-h-screen bg-blue-200 p-4">
+      <div className="w-full max-w-4xl bg-white rounded-lg shadow-lg p-6">
         <div className="flex flex-col gap-4 items-center">
-          <div className=" p-4 w-full max-w-md">
+          {/* Tabla de horarios */}
+          <div className="w-full max-w-md">
             <table className="w-full text-left border-collapse">
               <thead>
                 <tr>
@@ -31,25 +33,33 @@ const Contact = () => {
               </tbody>
             </table>
           </div>
-          <div className="p-2 w-full border-blue-500 flex items-center justify-center"> 
-          <Link href="https://wa.me/+526462160078" className='flex items-center justify-center m-2'>
-            <img src="logos/WSP.png" alt="Whatsapp Logo" className="w-80 h-30" />
-          </Link>
-          <Link href="https://mail.google.com/mail/u/1/#inbox?compose=GTvVlcRwRQQnbstJcLdRvRkMdBsSrspztBdZmBVlhpnJjctrwZQfhMKVbJBpcdrDtLCTBcWRlSPDw" className='flex items-center justify-center m-2'>
-                <img src='logos/gmail.png' alt='Gmail Logo' className='w-32 '></img>
-          </Link>
-          <Link href="https://www.facebook.com/Gregoyo55/">
-                <img src='logos/facebook.png' className='h-28'></img>
-          </Link>
-        </div>
-          <div className=" p-4 w-full max-w-md text-center font-bold">
+
+          {/* Enlaces de contacto */}
+          <div className="p-2 w-full flex flex-wrap items-center justify-center gap-4">
+            <Link href="https://wa.me/+526462160078" className="flex items-center justify-center m-2">
+              <img src="logos/WSP.png" alt="Whatsapp Logo" className="w-20 h-auto" />
+            </Link>
+            <Link href="https://mail.google.com/mail/u/1/#inbox?compose=GTvVlcRwRQQnbstJcLdRvRkMdBsSrspztBdZmBVlhpnJjctrwZQfhMKVbJBpcdrDtLCTBcWRlSPDw" className="flex items-center justify-center m-2">
+              <img src='logos/gmail.png' alt='Gmail Logo' className='w-20 h-auto'></img>
+            </Link>
+            <Link href="https://www.facebook.com/Gregoyo55/">
+              <img src='logos/facebook.png' className='h-20 w-auto'></img>
+            </Link>
+          </div>
+
+          {/* Dirección */}
+          <div className="p-4 w-full max-w-md text-center font-bold">
             Delante 332, Colonia Buenaventura, Ensenada, Baja California, México
           </div>
-          <div className='p-4 w-full border-blue-500 flex items-center justify-center'>
-        <img src='logos/phone.png' className='h-24'></img>
-        <h2 className='m-3 font-bold text-3xl'>646-120-56-05</h2>
-        </div>
-          <div className=" p-4 w-full">
+
+          {/* Teléfono */}
+          <div className='p-4 w-full flex items-center justify-center flex-col md:flex-row'>
+            <img src='logos/phone.png' className='h-16 md:h-24'></img>
+            <h2 className='m-3 font-bold text-2xl md:text-3xl'>646-120-56-05</h2>
+          </div>
+
+          {/* Mapa */}
+          <div className="p-4 w-full">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2849.8484075185834!2d-116.60067952288273!3d31.85058242834292!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d88df2460a130d%3A0xe8c59cb6f3737480!2sINTELEC!5e0!3m2!1ses-419!2smx!4v1720110123590!5m2!1ses-419!2smx"
               className="w-full h-64 md:h-96 border-0"
@@ -59,7 +69,6 @@ const Contact = () => {
             ></iframe>
           </div>
         </div>
-
       </div>
     </div>
   );
--- app/navbar/page.jsx
+++ app/navbar/page.jsx
@@ -1,52 +1,81 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Link from "next/link";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faHistory, faTools, faBoxOpen, faEnvelope, faCogs } from '@fortawesome/free-solid-svg-icons';
+import { 
+    faHome, 
+    faHistory, 
+    faTools, 
+    faBoxOpen, 
+    faEnvelope, 
+    faCogs, 
+    faBars, 
+    faTimes 
+} from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     const [hovered, setHovered] = useState(null);
-
-    const playHoverSound = () => {
-        const audio = new Audio('/sounds/click-sound.mp3'); // Ruta del archivo de audio
-        audio.play();
-    };
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const menuRef = useRef(null);
 
     const handleMouseEnter = (index) => {
         setHovered(index);
-        playHoverSound();
     };
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const handleClickOutside = (event) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setIsMenuOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (
-        <div>
-            <nav className="bg-white p-4 flex justify-between items-center">
-                <img className="h-24" src="/logos/intelec.png" alt="Logo Intelec" />
-                <ul className="flex space-x-4">
-                    {[
-                        { href: "/", label: "Inicio", icon: faHome },
-                        { href: "/historia", label: "Historia", icon: faHistory },
-                        { href: "/servicios", label: "Servicios", icon: faCogs },
-                        { href: "/productos", label: "Productos", icon: faBoxOpen },
-                        { href: "/about", label: "Contáctanos", icon: faEnvelope },
-                        { href: "/motores", label: "Herramientas", icon: faTools },
-                    ].map((item, index) => (
-                        <li key={index}>
-                            <Link href={item.href}>
-                                <button
-                                    onMouseEnter={() => handleMouseEnter(index)}
-                                    onMouseLeave={() => setHovered(null)}
-                                    className={`flex items-center p-3 rounded-lg transition-all duration-300 
-                                        ${hovered === index ? 'bg-indigo-500 text-white transform scale-105' : 'bg- text-gray-800 hover:bg-indigo-600'}`}
-                                >
-                                    <FontAwesomeIcon icon={item.icon} className="mr-2" />
-                                    {item.label}
-                                </button>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </div>
+        <nav className="bg-white p-4 flex justify-between items-center relative z-50">
+            <img className="h-24" src="/logos/intelec.png" alt="Logo Intelec" />
+            <button 
+                className="md:hidden p-2" 
+                onClick={toggleMenu}
+            >
+                <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
+            </button>
+            <ul 
+                ref={menuRef} 
+                className={`absolute md:static top-16 right-0 bg-white shadow-lg md:flex md:space-x-4 ${isMenuOpen ? "flex-col" : "hidden"} md:flex-row z-50`}
+            >
+                {[
+                    { href: "/", label: "Inicio", icon: faHome },
+                    { href: "/historia", label: "Historia", icon: faHistory },
+                    { href: "/servicios", label: "Servicios", icon: faCogs },
+                    { href: "/productos", label: "Productos", icon: faBoxOpen },
+                    { href: "/about", label: "Contáctanos", icon: faEnvelope },
+                    { href: "/motores", label: "Herramientas", icon: faTools },
+                ].map((item, index) => (
+                    <li key={index}>
+                        <Link 
+                            href={item.href} 
+                            onMouseEnter={() => handleMouseEnter(index)} 
+                            onMouseLeave={() => setHovered(null)} 
+                            className={`flex items-center p-3 rounded-lg transition-all duration-300 
+                                ${hovered === index ? 'bg-indigo-500 text-white transform scale-105' : 'bg-transparent text-gray-800 hover:bg-indigo-600'}`}
+                            onClick={() => setIsMenuOpen(false)} // Close menu on link click
+                        >
+                            <FontAwesomeIcon icon={item.icon} className="mr-2" />
+                            {item.label}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     );
 };
 
