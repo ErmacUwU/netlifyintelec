6f177d9d714a4d0dc932443bb829ddb75bec303f
Victor <al20760334@ite.edu.mx>
2024-10-26 11:48:36 -0700

    PLC

--- app/productos/plc/page.jsx
+++ app/productos/plc/page.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 
 const Plc = () => {
+
+  const catalogos = [
+    {
+      nombre: "KIT PLC EASY-E4",
+      url: "https://intelec332.github.io/catalogos/EASYE4.pdf",
+      thumbnail: "https://argoselectrica.com/wp-content/uploads/2024/01/PORTADILLAS-INDIVIDUAL-PAGINA-04-796x1024.jpg"
+    },
+    
+    // Añade más catálogos aquí
+  ];
+
   return (
-    <div>Plc</div>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Catálogos de Protecciones</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {catalogos.map((catalogo, index) => (
+          <div key={index} className="bg-white shadow-md rounded-lg p-4 flex flex-col items-center">
+            <img src={catalogo.thumbnail} alt={`Thumbnail de ${catalogo.nombre}`} className="w-32 h-32 object-cover mb-4 rounded-md hover:scale-110" />
+            <h2 className="text-lg font-semibold mb-2 text-center">{catalogo.nombre}</h2>
+            <Link href={catalogo.url} target="_blank" rel="noopener noreferrer">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
+                Ver Mas
+              </button>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
